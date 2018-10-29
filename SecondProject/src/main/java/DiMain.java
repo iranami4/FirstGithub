ff8eb97d8bcdeaec6496e19e5c8873183b212f83
user <user@user-PC>
2018-10-29 14:33:17 +0900

    3th commit

--- SecondProject/src/main/java/DiMain.java
+++ SecondProject/src/main/java/DiMain.java
@@ -3,7 +3,8 @@ public class DiMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//revert
+		// revert
+		// 리버트 하기
 		System.out.println("first : " + 1234);
 	}
 
